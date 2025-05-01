0f6440635be028fe235aba07c650cfa7fa02bd8d
trnkggt <gogotishvilitornike@yahoo.com>
2025-05-01 15:26:29 +0400

    chore: add dependencies

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlalchemy import text
 
-app = FastAPI()
+from .core import sessionmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    async with sessionmanager.connect() as conn:
+        await conn.execute(text("SELECT 1"))
+
+    yield
+
+    if sessionmanager._engine is not None:
+        await sessionmanager.close()
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
